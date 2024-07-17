bc6c8d2891be8182e6016591f4cf93afa1c96819
monster1935 <monster.1935.li@gmail.com>
2024-07-17 20:51:25 +0800

    feat: add demo

---
+++ public/lib/ad.js
@@ -0,0 +1,10 @@
+console.log('test')
+
+let script = document.createElement('script');
+script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8460202471959610';
+script.setAttribute("data-ad-frequency-hint", "30s");
+script.setAttribute("data-adbreak-test", "on");
+script.setAttribute('crossorigin', 'anonymous')
+
+document.getElementsByTagName('head')[0].appendChild(script);
+
